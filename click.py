a57548b278809081f685e3dc12c6b3cd0036262b
Daniel Boye <83395536+DanielBoye@users.noreply.github.com>
2023-07-05 22:48:18 +0200

    Update click.py

--- click.py
+++ click.py
@@ -1,10 +1,26 @@
 import os
 import click
 
-@click.command()
+
+@click.group()
+@click.option('--usb-path', type=click.Path(), help='Path to the USB drive')
+@click.pass_context
+def ducky_cli(ctx, usb_path):
+    """Hak5 USB Rubber Ducky CLI"""
+    ctx.ensure_object(dict)
+    ctx.obj['usb_path'] = usb_path
+
+
+@ducky_cli.command()
 @click.argument('file_path', type=click.Path(exists=True))
-@click.argument('usb_path', type=click.Path())
-def copy_to_usb(file_path, usb_path):
+@click.pass_context
+def copy_to_usb(ctx, file_path):
+    """Copy a payload from a text file to the USB Rubber Ducky"""
+    usb_path = ctx.obj.get('usb_path')
+    if not usb_path:
+        click.echo("USB path not provided. Use '--usb-path' option or set it using 'set-usb-path' command.")
+        return
+
     # Check if the file is a text file
     if not file_path.endswith('.txt'):
         click.echo(f"Invalid file type: {file_path} is not a text file (.txt)")
@@ -23,5 +39,15 @@ def copy_to_usb(file_path, usb_path):
     # Delete the temporary binary file
     os.remove(binary_file_path)
 
+
+@ducky_cli.command()
+@click.argument('usb_path', type=click.Path())
+@click.pass_context
+def set_usb_path(ctx, usb_path):
+    """Set the USB path for future commands"""
+    ctx.obj['usb_path'] = usb_path
+    click.echo(f"USB path set to: {usb_path}")
+
+
 if __name__ == '__main__':
-    copy_to_usb()
+    ducky_cli(obj={})
