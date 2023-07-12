d4d7daa69d835328a97bcd3f6e25625e1efc7d1a
DanielBoye <danielboye888@gmail.com>
2023-07-12 22:51:29 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='duckyshell',
+    version='0.0.7',
+    description='Hak5 USB Rubber Ducky CLI',
+    author='Daniel Boye',
+    author_email='danielboye888@gmail.com',
+    url='https://github.com/DanielBoye/duckyshell',
+    package_dir={'': 'duckyshell'},
+    scripts=['duckyshell/duckyshell.py'],
+
+)
