2ec197e7f7aca1545d83bafa3691f4ba55956ba7
DanielBoye <danielboye888@gmail.com>
2023-07-13 11:43:03 +0200

    Adding version, description and long_description as a variable

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
+VERSION = '0.0.7' 
+DESCRIPTION = 'Hak5 USB Rubber Ducky CLI'
+LONG_DESCRIPTION = 'A command line tool for copying and flashing the USB Rubber Ducky with new payloads from the command line'
+
 setup(
     name='duckyshell',
-    version='0.0.7',
-    description='Hak5 USB Rubber Ducky CLI',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     author='Daniel Boye',
     author_email='danielboye888@gmail.com',
     url='https://github.com/DanielBoye/duckyshell',
-    package_dir={'': 'duckyshell'},
-    scripts=['duckyshell/duckyshell.py'],
-
 )
