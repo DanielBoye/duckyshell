8383a434a2c8a1dad389f26df8bfcd33404d8d8a
DanielBoye <danielboye888@gmail.com>
2023-07-14 23:49:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo "Hello"
+# Clone the GitHub repository
+git clone https://github.com/DanielBoye/duckyshell.git
+
+# Move into the cloned repository
+
+# Copy the folder to /usr/bin
+sudo cp duckyshell /usr/bin/duckyshell
+
+# Create an alias for running duckyshell.py as 'ducky'
+echo "alias ducky='python3 /usr/bin/duckyshell.py'" >> ~/.bashrc
+
+# Reload the bashrc file to apply the alias
+source ~/.bashrc
+
+# Provide executable permissions to the program
+sudo chmod +x /usr/bin/duckyshell.py
+
+# Cleanup: remove the cloned repository
+rm -rf duckyshell
+
+# Installation completed
+echo "DuckyShell has been installed. You can now use 'ducky' command to run it."
