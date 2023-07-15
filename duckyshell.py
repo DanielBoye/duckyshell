435d599c761c6e28c324237c14c89b2fac340af6
DanielBoye <danielboye888@gmail.com>
2023-07-15 23:31:29 +0200

    Remove the convert t.xt to binary
    It does not work since Hak5 have their own type of encoding for the USB Rubber Ducky

--- duckyshell.py
+++ duckyshell.py
@@ -56,11 +56,6 @@ class DuckyShell(cmd.Cmd):
             print(f"Invalid file type: {self.file_path} is not a text file (.txt)")
             return
 
-        # Convert the text file to binary
-        binary_file_path = file_path[:-3] + 'bin'
-        with open(file_path, 'r') as text_file, open(binary_file_path, 'wb') as binary_file:
-            for line in text_file:
-                binary_file.write(line.encode('utf-8'))
 
         # Copy the binary file to the USB drive
         os.system(f"cp {binary_file_path} {self.usb_path}")
